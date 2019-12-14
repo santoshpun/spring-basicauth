7c97c69a6d9ff4656c12cd0460bd5396ff0e0e24
Santosh Pun <santosh@f1soft.com>
2019-12-14 14:14:13 +0545

    first commit

---
+++ src/main/java/com/santosh/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.santosh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Spring Security";
+	}
+}
